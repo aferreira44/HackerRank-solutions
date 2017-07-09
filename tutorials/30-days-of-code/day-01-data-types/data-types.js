75d81cd484d660a35443e6c26e5f49c560ff08e5
André Ferreira <contato@andreferreira.me>
2017-07-08 21:30:22 -0300

    refactor: use strict and semicolon

--- tutorials/30-days-of-code/day-01-data-types/data-types.js
+++ tutorials/30-days-of-code/day-01-data-types/data-types.js
@@ -1,11 +1,13 @@
+"use strict";
+
 // Receive the string "Welcome to 30 Days of Code!" as parameter
 // and print it after 'Hello, World'
 
 exports.print = (int, double, string) => {
-    const i = 4
-    const d = 4.0
-    const s = "HackerRank "
+    const i = 4;
+    const d = 4.0;
+    const s = "HackerRank ";
 
-    const result = i + int + "\n" + (d + double).toFixed(1) + "\n" + s + string
-    return result
+    const result = i + int + "\n" + (d + double).toFixed(1) + "\n" + s + string;
+    return result;
 }
